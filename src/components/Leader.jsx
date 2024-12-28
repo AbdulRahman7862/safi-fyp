6da3633b66b51e38ca30601f2eb19dd66276ae0c
Safi32 <shaheershahid75@gmail.com>
2024-12-28 10:00:00 +0500

    Clean up code

--- src/components/Leader.jsx
+++ src/components/Leader.jsx
@@ -1,2 +1,2 @@
-// Edited line at 33 on 2024-12-25
+// Edited line at 83 on 2024-12-28
 // Added line 43 on 2024-12-04
--- src/utils/valistors.js
+++ src/utils/valistors.js
@@ -1,3 +1,3 @@
-// Init src/utils/valistors.js
+// Edited line at 97 on 2024-12-28
 // Added line 8 on 2024-12-18
 // Added line 56 on 2024-12-27
