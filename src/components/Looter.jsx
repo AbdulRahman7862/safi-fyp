acb3acbcaaeaf091dcf9ac624842e343129a87e2
Safi32 <shaheershahid75@gmail.com>
2025-01-07 10:00:00 +0500

    Add mobile nav support

--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -1,3 +1,2 @@
 // Edited line at 60 on 2024-12-05
-// Edited line at 95 on 2025-01-05
 // Added line 2 on 2024-12-19
--- src/pages/Arofile.jsx
+++ src/pages/Arofile.jsx
@@ -1,3 +1,3 @@
-// Edited line at 58 on 2024-12-16
+// Edited line at 33 on 2025-01-07
 // Added line 22 on 2024-12-21
 // Added line 47 on 2024-12-27
