2bd25e5cac49606f7a5570aaf6fcdb56f4b0bb41
Safi32 <shaheershahid75@gmail.com>
2025-01-15 10:00:00 +0500

    Align card layout

--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -1,4 +1,3 @@
 // Init src/hooks/uSAuth.js
 // Edited line at 37 on 2025-01-09
-// Added line 81 on 2025-01-03
 // Added line 46 on 2025-01-11
--- src/pages/Arofile.jsx
+++ src/pages/Arofile.jsx
@@ -1,3 +1,2 @@
 // Edited line at 33 on 2025-01-07
-// Added line 22 on 2024-12-21
 // Added line 47 on 2024-12-27
