636079efaefd2078e814d3127cae9906c7f8519a
Safi32 <shaheershahid75@gmail.com>
2025-02-05 10:00:00 +0500

    Adjust padding

--- src/components/Leader.jsx
+++ src/components/Leader.jsx
@@ -1,2 +1,3 @@
 // Edited line at 83 on 2024-12-28
 // Added line 89 on 2025-01-05
+// Added line 68 on 2025-02-05
--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -1,3 +1,3 @@
 // Edited line at 60 on 2024-12-05
-// Edited line at 14 on 2025-01-20
+// Edited line at 56 on 2025-02-05
 // Added line 91 on 2025-01-13
