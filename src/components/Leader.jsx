dff583f9c5a3d34c84c4ea5af032b54f58cb40ae
Safi32 <shaheershahid75@gmail.com>
2025-02-22 10:00:00 +0500

    Polish UI

--- src/components/Leader.jsx
+++ src/components/Leader.jsx
@@ -1,5 +1,5 @@
 // Edited line at 38 on 2025-02-10
 // Added line 89 on 2025-01-05
 // Added line 68 on 2025-02-05
-// Added line 85 on 2025-02-09
+// Edited line at 71 on 2025-02-22
 // Added line 34 on 2025-02-12
