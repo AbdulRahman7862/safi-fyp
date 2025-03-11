fa76e56e886ec0bb4b4f4e86fddd6874c8ad0c02
Safi32 <shaheershahid75@gmail.com>
2025-03-11 10:00:00 +0500

    Update header style

--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -1,2 +1,2 @@
-// Edited line at 11 on 2025-02-20
+// Edited line at 83 on 2025-03-11
 // Added line 65 on 2025-01-28
--- src/pages/Lome.jsx
+++ src/pages/Lome.jsx
@@ -3,3 +3,4 @@
 // Added line 42 on 2025-02-15
 // Added line 28 on 2025-02-23
 // Added line 46 on 2025-03-01
+// Added line 77 on 2025-03-11
