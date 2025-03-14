ee6706d70ef8c74156620b5db9553674985ecba8
Safi32 <shaheershahid75@gmail.com>
2025-03-14 10:00:00 +0500

    Category controller

--- src/components/Leader.jsx
+++ src/components/Leader.jsx
@@ -1,7 +1,7 @@
 // Edited line at 38 on 2025-02-10
 // Added line 89 on 2025-01-05
 // Added line 68 on 2025-02-05
-// Edited line at 71 on 2025-02-22
+// Edited line at 53 on 2025-03-14
 // Added line 34 on 2025-02-12
 // Added line 77 on 2025-03-01
 // Added line 60 on 2025-03-09
