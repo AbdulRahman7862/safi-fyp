c07ecc4a20d2ad92ff81c768abbe45306223d640
Safi32 <shaheershahid75@gmail.com>
2025-03-16 10:00:00 +0500

    Align card layout

--- src/components/Leader.jsx
+++ src/components/Leader.jsx
@@ -1,5 +1,4 @@
 // Edited line at 38 on 2025-02-10
-// Added line 89 on 2025-01-05
 // Added line 68 on 2025-02-05
 // Edited line at 53 on 2025-03-14
 // Added line 34 on 2025-02-12
--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -1,3 +1,3 @@
-// Init src/hooks/uSAuth.js
+// Edited line at 41 on 2025-03-16
 // Edited line at 44 on 2025-03-09
 // Added line 65 on 2025-01-23
