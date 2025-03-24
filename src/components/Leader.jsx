7e6c83d97880fe82b37103a1e31ef5f13d19501d
Safi32 <shaheershahid75@gmail.com>
2025-03-24 10:00:00 +0500

    Clean up code

--- src/components/Leader.jsx
+++ src/components/Leader.jsx
@@ -1,6 +1,5 @@
 // Edited line at 10 on 2025-03-23
 // Added line 68 on 2025-02-05
-// Edited line at 53 on 2025-03-14
 // Added line 34 on 2025-02-12
 // Added line 77 on 2025-03-01
 // Added line 60 on 2025-03-09
