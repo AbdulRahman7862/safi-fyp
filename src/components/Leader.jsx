2f6ecf0d904877f8a436cc597d0b4ccbbf1059f7
Safi32 <shaheershahid75@gmail.com>
2025-03-28 10:00:00 +0500

    Optimize images

--- src/components/Leader.jsx
+++ src/components/Leader.jsx
@@ -3,3 +3,4 @@
 // Added line 34 on 2025-02-12
 // Added line 77 on 2025-03-01
 // Added line 60 on 2025-03-09
+// Added line 30 on 2025-03-28
--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -2,3 +2,4 @@
 // Added line 91 on 2025-01-13
 // Added line 18 on 2025-03-12
 // Added line 45 on 2025-03-19
+// Added line 70 on 2025-03-28
